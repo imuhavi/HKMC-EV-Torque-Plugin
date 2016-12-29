e80b09f628cd4e8f0e58aad2a9259833f2345960
Wookhyun SHIN <wshin@satreci.com>
2016-12-29 12:44:02 +0900

    Update add PID function. PIDs added by plugin is now accessible by "Manage extra PIDs/sensor >> Add predefined set >> HKMC EV". This is how Hyundai ADV EX works.

--- app/src/main/java/jejusoul/com/github/obd_pids_for_hkmc_evs/PluginActivity.java
+++ app/src/main/java/jejusoul/com/github/obd_pids_for_hkmc_evs/PluginActivity.java
@@ -16,48 +16,33 @@ import org.prowl.torque.remote.ITorqueService;
 public class PluginActivity extends Activity {
 
     private ITorqueService torqueService;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        findViewById(R.id.soul_since_2015).setOnClickListener(mClickListener);
-
-
-        Intent intent = new Intent();
-        intent.setClassName("org.prowl.torque", "org.prowl.torque.remote.TorqueService");
-        boolean successfulBind = bindService(intent, connection, 0);
-
-        if (successfulBind) {
-
-            // Not really anything to do here.  Once you have bound to the service, you can start calling
-            // methods on torqueService.someMethod()  - look at the aidl file for more info on the calls
-
-        } else {
-
-            Toast.makeText(getApplicationContext(), "Fail to bind torque service", Toast.LENGTH_SHORT).show();
-        }
-
-    }
-
     Button.OnClickListener mClickListener = new View.OnClickListener() {
         public void onClick(View v) {
-            Toast.makeText(getApplicationContext(), "Blahblah", Toast.LENGTH_SHORT).show();
 
             try {
-                torqueService.setPIDInformation("TEST PID", "TPID", "Unit", 1000, 0, 0, "0");
-//                torqueService.sendPIDDataV2("myplugin",
-//                        new String[]{"AAA"}, new String[]{"A"}, new String[]{"2101"}, new String[]{"A*100"}, new float[]{0}, new float[]{100}, new String[]{"Unit"}, new String[]{"7E4"}, new String[]{"A"}, new String[]{"B"});
+                // Manage extra PIDs/Sensors >> Add predefined set >> HKMC EV
+                // TODO: Categorize by vehicle model
+                boolean success = torqueService.sendPIDData("HKMC EV",
+                        new String[]{"XXXXX"},
+                        new String[]{"TPID"},
+                        new String[]{"2101"},
+                        new String[]{"A*100"},
+                        new float[]{0.0f},
+                        new float[]{1000.0f},
+                        new String[]{"mA"},
+                        new String[]{"Auto"});
+
+                if (!success) {
+                    Toast.makeText(getApplicationContext(), "Fail to add PID data", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Successfully added PID data", Toast.LENGTH_SHORT).show();
+                }
 
             } catch (RemoteException ex) {
                 Toast.makeText(getApplicationContext(), ex.toString(), Toast.LENGTH_SHORT).show();
             }
         }
     };
-
-
     /**
      * Bits of service code. You usually won't need to change this.
      */
@@ -83,4 +68,29 @@ public class PluginActivity extends Activity {
 
         ;
     };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.soul_since_2015).setOnClickListener(mClickListener);
+
+
+        Intent intent = new Intent();
+        intent.setClassName("org.prowl.torque", "org.prowl.torque.remote.TorqueService");
+        boolean successfulBind = bindService(intent, connection, 0);
+
+        if (successfulBind) {
+
+            // Not really anything to do here.  Once you have bound to the service, you can start calling
+            // methods on torqueService.someMethod()  - look at the aidl file for more info on the calls
+
+        } else {
+
+            Toast.makeText(getApplicationContext(), "Fail to bind torque service", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 }
