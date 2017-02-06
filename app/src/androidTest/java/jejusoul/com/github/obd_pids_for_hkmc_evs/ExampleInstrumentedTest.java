287512bd70bd96a1d4364e9fa4123639f0c7e85d
ayokyun <wookhyun.shin@gmail.com>
2017-02-07 01:08:46 +0900

    Add Soul sample PIDs

--- app/src/androidTest/java/jejusoul/com/github/obd_pids_for_hkmc_evs/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/jejusoul/com/github/obd_pids_for_hkmc_evs/ExampleInstrumentedTest.java
@@ -1,11 +1,18 @@
 package jejusoul.com.github.obd_pids_for_hkmc_evs;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.opencsv.CSVReader;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
 
 import static org.junit.Assert.*;
 
@@ -22,5 +29,22 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("jejusoul.com.github.obd_pids_for_hkmc_evs", appContext.getPackageName());
+
+
+        File filesDir = appContext.getFilesDir();
+
+        AssetManager assetManager = appContext.getAssets();
+        String[] files = assetManager.list("");
+
+
+
+        for(String filename : files) {
+
+            CSVReader reader = new CSVReader(new BufferedReader(new InputStreamReader(appContext.getAssets().open("SoulEV/Kia_Soul_EV_Battery_Cell_data.csv"))));
+            String[] nextLine;
+        }
+
+        assertEquals("jejusoul.com.github.obd_pids_for_hkmc_evs", appContext.getPackageName());
     }
+
 }
